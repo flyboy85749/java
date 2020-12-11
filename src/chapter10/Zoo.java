8000a66d94507efde398f0f455aeceb368fd32db
William Hall <billhall168@gmail.com>
2020-12-11 04:12:14 -0700

    Begin optional polymorphism exercise with fruits

--- src/chapter10/Zoo.java
+++ src/chapter10/Zoo.java
@@ -5,5 +5,27 @@ public class Zoo {
         Dog Rocky = new Dog();
         Rocky.fetch();
         Rocky.makeSound();
+        feed(Rocky);
+
+        Animal Sasha = new Dog();
+        Sasha.makeSound();
+        feed(Sasha);
+
+        Sasha = new Cat();
+        Sasha.makeSound();
+        // casting example
+        ((Cat) Sasha).scratch();
+        feed(Sasha);
+
+    }
+
+    public static void feed (Animal animal){
+    if (animal instanceof Dog) {
+        System.out.println("Here's your dog food.");
+    }
+
+    else if (animal instanceof Cat){
+        System.out.println("Here's your cat food.");
+    }
     }
 }
