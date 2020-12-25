cee6130439a37ea29f7197e5b30990cbc37acc1f
William Hall <billhall168@gmail.com>
2020-12-25 05:21:20 -0700

    exception handling with finally

--- src/chapter13/ExceptionHandling.java
+++ src/chapter13/ExceptionHandling.java
@@ -2,20 +2,40 @@ package chapter13;
 
 import java.awt.image.ImagingOpException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionHandling {
-    public static void main(String [] args){
-        createNewFile();
+    public static void main(String[] args) {
+//        createNewFile();
+        numbersExceptionHandling();
     }
 
-    public static void createNewFile(){
+    public static void createNewFile() {
         File file = new File("resources/nonexistent.txt");
         try {
             file.createNewFile();
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Directory does not exist.");
             e.printStackTrace();
+        }
+    }
+
+    public static void numbersExceptionHandling() {
+        File file = new File("resources/numbers.txt");
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(file);
+            while (fileReader.hasNext()) {
+                double num = fileReader.nextDouble();
+                System.out.println(num);
+            }
+        } catch (FileNotFoundException | InputMismatchException e) {
+            e.printStackTrace();
+        } finally {
+            fileReader.close();
         }
     }
 }
